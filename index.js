97229ebdaa1f041c45ffaf971cde21ec2fa1107c
Eliju <36505898+Eliju@users.noreply.github.com>
2020-12-12 22:51:00 +0200

    3.9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const e = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function getBody(req) {
     if(req.method === 'POST'){
@@ -116,7 +118,7 @@ app.post('/api/persons', (req,res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
