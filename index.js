94bdb79843d0b0c2b209cb89bc9d7aa291a53061
Eliju <36505898+Eliju@users.noreply.github.com>
2020-12-14 17:13:48 +0200

    3.16

--- index.js
+++ index.js
@@ -143,6 +143,21 @@ app.post('/api/persons', (req,res,next) => {
     }
 })
 
+app.put('/api/persons/:id', (req,res,next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
 const unknownEndPoint = (req,res) => {
     res.status(404).send({error: 'Unknown endpoint'})
 }
